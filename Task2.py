bd8323cf14ad2278d588374ec4ac97ab82f0efc3
Vivan-23 <137032932+Vivan-23@users.noreply.github.com>
2024-08-01 10:51:21 +0530

    Add files via upload

---
+++ Task2.py
@@ -0,0 +1,14 @@
+gadgets = [
+    ("Explosive Batarangs", 3, True),
+    ("Batarangs", 5, True),
+    ("Smoke Pellets", 0, False),
+    ("Tear Gas Grenades", 2, True),
+    ("Night Vision Goggles", 1, True),
+    ("Batclaw", 0, False),
+    ("Grapple Gun", 3, True),
+    ("Batsignal", 0, False),
+    ("Utility Belt", 1, True),
+    ("Batmobile Tires", 4, True)
+]
+print(sorted(gadgets, key=lambda gadget: (-gadget[2], -gadget[1], gadget[0])))
+
